83b51adcd699314e4c9b47776c81493bd2d041cf
Shivupujari2003 <shivu.kp.333@gmail.com>
2025-10-27 20:05:23 +0530

    Header completed

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+// import Home from "./pages/Home";
+// import About from "./pages/About";
+// import Services from "./pages/Services";
+// import Contact from "./pages/Contact";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+        {/* <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} /> */}
+      </Routes>
+    </>
+  );
+}
